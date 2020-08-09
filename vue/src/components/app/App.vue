a1a743c86930c8503a9bd4ba645db8b394e58207
yakovliam <jacob@yakovliam.com>
2020-08-09 16:41:44 -0400

    Finish up sockets & message displaying

--- vue/src/components/app/App.vue
+++ vue/src/components/app/App.vue
@@ -11,9 +11,8 @@
       <div class="row chat-row">
         <!---------------------->
 
-        <div class="col chat-column float-1">
+        <div class="col chat-column float-1" id="chat-column">
           <div id="messages-container" class="chat-container">
-            <div id="style-1" class="scrollbar"></div>
           </div>
         </div>
 
@@ -46,10 +45,7 @@
 
           <div class="send-button-container">
             <button v-on:click="sendMessage" class="send-button">
-            <span class="circle" aria-hidden="true">
-              <span class="icon arrow"></span>
-            </span>
-              <span class="button-text">Send</span>
+              &#187;&#187;&#187;
             </button>
           </div>
         </div>
@@ -73,7 +69,14 @@ export default {
   name: 'App',
   methods: {
     sendMessage: function () {
-      this.$socket.emit('message', {"message": this.$refs.textInput.value})
+      const message = this.$refs.textInput.value;
+
+      // if message empty, don't send!
+      if (message === undefined || message === "") {
+        return;
+      }
+
+      this.$socket.emit('message', {"message": message})
 
       // clear text area
       this.$refs.textInput.value = "";
@@ -95,12 +98,48 @@ export default {
       // message
       const message = data.message;
 
+      let isSelf = username === this.username;
+
+      // create wrapper
+      let wrapper = document.createElement("div");
+
+      // check if the message is being sent by us or someone else
+      if (isSelf) {
+        // self
+        wrapper.className = "chat-message-self-wrap";
+      } else {
+        // other
+        wrapper.className = "chat-message-other-wrap";
+      }
+
       // create new message
       let divMessage = document.createElement('div');
       divMessage.innerHTML = "<b>" + username + "</b>: " + message;
-      divMessage.className = "chat-message";
 
-      document.getElementById("messages-container").appendChild(divMessage);
+      // check if the message is being sent by us or someone else
+      if (isSelf) {
+        // self
+        divMessage.className = "chat-message-self";
+      } else {
+        // other
+        divMessage.className = "chat-message-other";
+      }
+
+      // add message to wrapper
+      wrapper.appendChild(divMessage);
+
+      // create break
+      let brk = document.createElement("br");
+
+      // get container
+      const container = document.getElementById("messages-container");
+
+      // append wrapper (which has message)!
+      container.appendChild(wrapper);
+      container.appendChild(brk);
+
+      // automatically scroll to bottom of chat column
+      document.getElementById("chat-column").scrollTop = document.getElementById("chat-column").scrollHeight;
     },
     leave(data) {
       this.totalCount = data.totalCount;
